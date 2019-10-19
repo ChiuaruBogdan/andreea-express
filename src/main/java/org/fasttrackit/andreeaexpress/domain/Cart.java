27cffa2b0c16167046fee68a0a35ea2e18752d59
Chiuaru Bogdan <bchiuaru94@gmail.com>
2019-10-19 13:11:15 +0300

    Adaugat "metode utilitare"

--- src/main/java/org/fasttrackit/andreeaexpress/domain/Cart.java
+++ src/main/java/org/fasttrackit/andreeaexpress/domain/Cart.java
@@ -3,6 +3,8 @@ package org.fasttrackit.andreeaexpress.domain;
 import org.hibernate.annotations.Fetch;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Cart {
@@ -11,6 +13,25 @@ public class Cart {
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
     private Customer customer;
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "cart_products",
+            joinColumns = @JoinColumn(name = "cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private Set<Product> products = new HashSet<>();
+
+    public void addToCart(Product product){
+        products.add(product);
+
+        product.getCarts().add(this);
+    }
+
+    public void removeFromCart(Product product){
+        products.remove(product);
+
+        product.getCarts().remove(this);
+    }
+
 
     public long getId() {
         return id;
@@ -24,7 +45,32 @@ public class Cart {
         return customer;
     }
 
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
     public void setCustomer(Customer customer) {
         this.customer = customer;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cart cart = (Cart) o;
+
+        return id == cart.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
     }
 }
--- src/test/java/org/fasttrackit/andreeaexpress/CartServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/andreeaexpress/CartServiceIntegrationTests.java
@@ -1,6 +1,9 @@
 package org.fasttrackit.andreeaexpress;
 
+import org.fasttrackit.andreeaexpress.domain.Customer;
 import org.fasttrackit.andreeaexpress.service.CartService;
+import org.fasttrackit.andreeaexpress.steps.CustomerSteps;
+import org.fasttrackit.andreeaexpress.transfer.cart.AddProductToCartRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +16,18 @@ public class CartServiceIntegrationTests {
 
     @Autowired
     private CartService cartService;
+    @Autowired
+    private CustomerSteps customerSteps;
 
     @Test
     public void testAddToCart_WhenNewCart_thenCreateCart() {
+        Customer customer = customerSteps.createCustomer();
 
+        AddProductToCartRequest request = new AddProductToCartRequest();
+        request.setCustomerId(customer.getId());
+//        TODO: replace this after mapping Cart-Product relantionship
+        request.setProductId(10L);
+
+        cartService.addProductToCart(request);
     }
 }
